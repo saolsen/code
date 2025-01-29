f638f72bcd04d8c593216580fb94e5180c11fe4d
steve olsen <steve.olsen@close.com>
2025-01-29 12:07:32 -0600

    bigints

--- lab/bigints.c
+++ lab/bigints.c
@@ -1,35 +1,26 @@
-// Since on 64-bit systems you always have sse2 or neon, can you just always use those for 128 bit
-// ints?
 #include <stdint.h>
 #include <stdio.h>
+#include <inttypes.h>
 
-#if defined(__x86_64__) || defined(_M_X64) || defined(i386) || defined(_M_IX86)
-#include <emmintrin.h> // SSE2 headers
+// 128 big unsigned integer just works on 64-bit systems with clang/gcc.
+// still need to figure it out for msvc.
 
-#elif defined(__ARM_NEON) || defined(__aarch64__)
-#include <arm_neon.h> // NEON headers
-
-#else
-#error "Unsupported Platform"
-#endif
+typedef uint8_t     u8;
+typedef int32_t     i32;
+typedef uint64_t    u64;
+typedef __uint128_t u128;
 
 int main(void) {
-    // Do 128 bit ints just work?
-    // instead of uint_8, this should be uint2_t, 00, 01, 10, 11
-    // uint8_t seed[64] = {0,1,2,3,0,1,2,3,
-    //                     0,1,2,3,0,1,2,3,
-    //                     0,1,2,3,0,1,2,3,
-    //                     0,1,2,3,0,1,2,3,
-    //                     0,1,2,3,0,1,2,3,
-    //                     0,1,2,3,0,1,2,3,
-    //                     0,1,2,3,0,1,2,3,
-    //                     0,1,2,3,0,1,2,3,
-    //                     };
-    //
-    // __uint128_t a = 0x123456789abcdef0;
-    //
-    // uint16_t seed_rows[8] = {0,1,2,3,4,5,6,7};
-    //
-    // printf("Big Int: %lld\n", a);
+    u128 a = (((u128)100000004294967294 << 64) | 4294967294);
+    //a += 1;
+
+    // Print the hex integer
+    u64 high = (u64)(a >> 64);
+    u64 low = (u64)a & 0xffffffffffffffff;
+    // 32 digits.
+    printf("Hex:       0x%016" PRIx64 "%016" PRIx64 "\n", high, low);
+    // 40 digits.
+    printf("Dec: %20.0" PRIu64 "%020" PRIu64 "\n", high, low);
+
     return 0;
 }
