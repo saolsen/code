1830d5a972cfb6ba77248fa0fc0794d58a4d3237
steve olsen <steve@steve.computer>
2025-01-28 20:12:28 -0600

    not used

--- lab/bigints.c
+++ lab/bigints.c
@@ -1,35 +1,35 @@
-// Since on 64-bit systems you always have sse2 or neon, can you just always use those for 128 bit ints?
-#include <stdio.h>
+// Since on 64-bit systems you always have sse2 or neon, can you just always use those for 128 bit
+// ints?
 #include <stdint.h>
+#include <stdio.h>
 
 #if defined(__x86_64__) || defined(_M_X64) || defined(i386) || defined(_M_IX86)
-#include <emmintrin.h>  // SSE2 headers
+#include <emmintrin.h> // SSE2 headers
 
 #elif defined(__ARM_NEON) || defined(__aarch64__)
-#include <arm_neon.h>   // NEON headers
+#include <arm_neon.h> // NEON headers
 
 #else
 #error "Unsupported Platform"
 #endif
 
-
 int main(void) {
     // Do 128 bit ints just work?
     // instead of uint_8, this should be uint2_t, 00, 01, 10, 11
-    uint8_t seed[64] = {0,1,2,3,0,1,2,3,
-                        0,1,2,3,0,1,2,3,
-                        0,1,2,3,0,1,2,3,
-                        0,1,2,3,0,1,2,3,
-                        0,1,2,3,0,1,2,3,
-                        0,1,2,3,0,1,2,3,
-                        0,1,2,3,0,1,2,3,
-                        0,1,2,3,0,1,2,3,
-                        };
-
-    __uint128_t a = 0x123456789abcdef0;
-
-    uint16_t seed_rows[8] = {0,1,2,3,4,5,6,7};
-
-    printf("Big Int: %lld\n", a);
+    // uint8_t seed[64] = {0,1,2,3,0,1,2,3,
+    //                     0,1,2,3,0,1,2,3,
+    //                     0,1,2,3,0,1,2,3,
+    //                     0,1,2,3,0,1,2,3,
+    //                     0,1,2,3,0,1,2,3,
+    //                     0,1,2,3,0,1,2,3,
+    //                     0,1,2,3,0,1,2,3,
+    //                     0,1,2,3,0,1,2,3,
+    //                     };
+    //
+    // __uint128_t a = 0x123456789abcdef0;
+    //
+    // uint16_t seed_rows[8] = {0,1,2,3,4,5,6,7};
+    //
+    // printf("Big Int: %lld\n", a);
     return 0;
 }
