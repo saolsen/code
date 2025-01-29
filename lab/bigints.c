884d49b4620217c4668551b602947175c185b2a0
steve olsen <steve@steve.computer>
2025-01-28 20:11:15 -0600

    dont test bigints

--- lab/bigints.c
+++ lab/bigints.c
@@ -1,5 +1,6 @@
 // Since on 64-bit systems you always have sse2 or neon, can you just always use those for 128 bit ints?
 #include <stdio.h>
+#include <stdint.h>
 
 #if defined(__x86_64__) || defined(_M_X64) || defined(i386) || defined(_M_IX86)
 #include <emmintrin.h>  // SSE2 headers
@@ -10,7 +11,6 @@
 #else
 #error "Unsupported Platform"
 #endif
-
 
 
 int main(void) {
