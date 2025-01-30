95e994ed7ca3b6cd35e998f08815c9c220de0e32
steve olsen <steve.olsen@close.com>
2025-01-30 16:14:01 -0600

    a whole bunch of work on hex coordinates and drawing

--- tazar/tazar_game.c
+++ tazar/tazar_game.c
@@ -1,5 +1,2 @@
-// A really basic implementation of Tazar and an AI player, so I have someone to play with.
-// https://kelleherbros.itch.io/tazar
-// Been running this alongside and inputting all the moves into the real game, that's why it
-// asks for the results of Volley rolls. Not really meant to be "played" standalone.
+
 
